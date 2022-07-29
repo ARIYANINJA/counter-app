27be932b4f0eb11fc4f1a2663a6eed2d18e931b9
Ariyaninj <ariyaninj@gmail.com>
2022-07-30 02:16:53 +0800

    doneLastCommit

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -2,18 +2,19 @@ import React, { Component } from 'react';
 import Counter from './counter.jsx'
 class Counters extends Component {
     render() { 
+        const {onReset, onIncrement, onDelete, counters} = this.props;
         return (
             <React.Fragment>
                 <button
-                onClick={this.props.onReset}
+                onClick={onReset}
                  className="btn btn-primary btn-sm m-2">
                     Reset
                 </button>
-            {this.props.counters.map(counter => (
+            {counters.map(counter => (
                 <Counter 
                 key={counter.id}
-                onDelete = {this.props.onDelete}
-                onIncrement = {this.props.onIncrement}
+                onDelete = {onDelete}
+                onIncrement = {onIncrement}
                 counter = {counter}
                 />
                 ))}
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
-class NavBar extends Component {
-    render() { 
-        return (
-            <div>
-               <nav className="navbar bg-light">
-                <div className="container-fluid">
-                <a className="navbar-brand" href="#">Navbar
-                <span className='badge bg-secondary m-2'>{this.props.totalCounters}</span>
-                </a>
-                </div>
-                </nav>
+const NavBar = ({totalCounters}) =>{
+    return (
+        <div>
+           <nav className="navbar bg-light">
+            <div className="container-fluid">
+            <a className="navbar-brand" href="#">Navbar
+            <span className='badge bg-secondary m-2'>{totalCounters}</span>
+            </a>
             </div>
-        );
-    }
-}
+            </nav>
+        </div>
+    );
+};
  
 export default NavBar;
